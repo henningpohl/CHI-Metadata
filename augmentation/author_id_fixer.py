e1e2710bf67f0d4656959b59b0715dbea802e776
Henning Pohl <HenningPohl@gmail.com>
2023-07-29 20:24:12 +0200

    Applying a bunch of updated acmids of authors, as these seem to not be persistent and change

--- augmentation/author_id_fixer.py
+++ augmentation/author_id_fixer.py
@@ -1,16 +1,35 @@
 import pandas as pd
-from augment import augment_paper_author
+from augment import augment_paper_author, augment_papers_with
 
-def iter_fixes():
+def apply_fixes():
     data = pd.read_csv('author_id_fixes.csv')
     for row in data.itertuples():
-        yield row
-
-if __name__ == '__main__':
-    for fix in iter_fixes():      
         augment_paper_author(
             fix.year,
             fix.doi,
             dict(name=fix.name),
             dict(acmid=fix.acmid, orcid=fix.orcid))
-        print(f'Updated info for {fix.name}') 
+        print(f'Updated info for {fix.name}')
+
+# Hacky fixes for acmids that were remapped later on
+def recode_acmids():
+    recodes = pd.read_csv('acmid_updates.csv')
+    code_map = {str(x.old_id): str(x.new_id) for x in recodes.itertuples()}
+    
+    def recode_ids(data):
+        changes = 0
+        for author in data['authors']:
+            if author['acmid'] in code_map:
+                changes = changes + 1
+                author['acmid'] = code_map[author['acmid']]
+                print('changed', data['doi'])
+        return (changes, data)
+    augment_papers_with(recode_ids)
+        
+if __name__ == '__main__':
+    apply_fixes()
+    recode_acmids()
+
+
+    
+    
