321a3324582a9d44e691bc7d32edb1203f7eb29c
Henning Pohl <HenningPohl@gmail.com>
2025-05-28 17:44:50 +0200

    Two name fixes added

--- analysis/check_names.py
+++ analysis/check_names.py
@@ -1,23 +1,42 @@
 import pandas as pd
 from data import paper_iter
 
-initials_cases = []
+cases = []
 for year, doi, paper in paper_iter():
     for author in paper['authors']:
         parts = author['name'].split(' ')
         # just initials first name
         initials_only = len([p for p in parts if '.' in p])
-        
-        if len(parts) - initials_only < 2:
-            initials_cases.append({
+
+        # The 1% shortest ones
+        if len(author['name']) < 8:
+            cases.append({
                 'year': paper['year'],
                 'doi': paper['doi'],
-                'acmid': author['acmid'],
+                'acmid': str(author['acmid']),
                 'name': author['name'],
                 'newName': 'TODO'
             })
-initials_cases = pd.DataFrame(initials_cases)
-initials_cases.sort_values('acmid', inplace=True)
-initials_cases.to_csv('name_fixes.csv', index=False)
-print(initials_cases) 
+        # The 1% longest ones
+        elif len(author['name']) > 24:
+            cases.append({
+                'year': paper['year'],
+                'doi': paper['doi'],
+                'acmid': str(author['acmid']),
+                'name': author['name'],
+                'newName': 'TODO'
+            })
+        # Initials only ones
+        elif len(parts) - initials_only < 2:
+            cases.append({
+                'year': paper['year'],
+                'doi': paper['doi'],
+                'acmid': str(author['acmid']),
+                'name': author['name'],
+                'newName': 'TODO'
+            })
+cases = pd.DataFrame(cases)
+cases.sort_values('acmid', inplace=True)
+cases.to_csv('name_fixes.csv', index=False)
+print(cases) 
 
